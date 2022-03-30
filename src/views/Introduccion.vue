045ea13dc690cb0ebcb93956503beb33b01b1ae4
DiegoVelasco84 <83100265+DiegoVelasco84@users.noreply.github.com>
2022-03-30 16:45:10 -0500

    avance 8

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -24,7 +24,7 @@
     
     figure.mb-5
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/8AI-QmhqzNg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="560" height="315" src="https://youtu.be/n9YnrpJvYWM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
     separador
 
 </template>
