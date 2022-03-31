f201dd6e8ebda612c0c02429544a3512c5058c47
DiegoVelasco84 <83100265+DiegoVelasco84@users.noreply.github.com>
2022-03-31 11:17:29 -0500

    video ajustado

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -24,7 +24,7 @@
     
     figure.mb-5
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/zeDQeU91fTQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/My9qe3ZMG0Q" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
     separador
 
 </template>
